8ce03979d0722d36f9b13a8122abec5e97ed7e45
楊野勇智 <yuji-1124-1984@Mac-Pro.local>
2017-04-10 21:48:25 +0900

    test show user profile

--- app/controllers/line_controller.rb
+++ app/controllers/line_controller.rb
@@ -11,6 +11,14 @@ class LineController < ApplicationController
     request_body = "grant_type=authorization_code&code=#{code}&client_id=#{ENV['client_id']}&client_secret=#{ENV['client_secret']}&redirect_uri=#{ENV['redirect_uri']}"
     response = RestClient.post 'https://api.line.me/v2/oauth/accessToken', request_body, {content_type: 'application/x-www-form-urlencoded'}
     @response_json = response.body
+    access_token = @response_json['access_token']
+    token_type = @response_json['token_type']
+    refresh_token = @response_json['refresh_token']
+    profile_response = RestClient.get 'https://api.line.me/v2/profile', {'Authorization' => "#{token_type} #{access_token}"}
+    @profile_response_json = profile_response.body
+    @userId = @profile_response_json['userId']
+    @display_name = @profile_response_json['displayName']
+    @picture_url = @profile_response_json['pictureUrl']
   end
 
 end
