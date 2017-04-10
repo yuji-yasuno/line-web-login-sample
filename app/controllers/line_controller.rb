b241a5014b6275f71e5b7ad6d1c43ede23e4601a
楊野勇智 <yuji-1124-1984@Mac-Pro.local>
2017-04-10 22:17:01 +0900

    test 6

--- app/controllers/line_controller.rb
+++ app/controllers/line_controller.rb
@@ -11,15 +11,15 @@ class LineController < ApplicationController
     request_body = "grant_type=authorization_code&code=#{code}&client_id=#{ENV['client_id']}&client_secret=#{ENV['client_secret']}&redirect_uri=#{ENV['redirect_uri']}"
     response = RestClient.post 'https://api.line.me/v2/oauth/accessToken', request_body, {content_type: 'application/x-www-form-urlencoded'}
 
-    @response_json = response.body
+    @response_json = JSON.parse(response.body)
     logger.debug("@response_json = #{@response_json}")
 
-    access_token = @response_json['access_token']
-    token_type = @response_json['token_type']
-    refresh_token = @response_json['refresh_token']
-    logger.debug(access_token)
-    logger.debug(token_type)
-    logger.debug(refresh_token)
+    @access_token = @response_json['access_token']
+    @token_type = @response_json['token_type']
+    @refresh_token = @response_json['refresh_token']
+    logger.debug(@access_token)
+    logger.debug(@token_type)
+    logger.debug(@refresh_token)
 
   end
 
