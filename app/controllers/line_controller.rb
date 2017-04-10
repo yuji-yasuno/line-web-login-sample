2f5be0b19768a61125ea5773e68bfd52acb7bb28
楊野勇智 <yuji-1124-1984@Mac-Pro.local>
2017-04-10 21:58:36 +0900

    test 2

--- app/controllers/line_controller.rb
+++ app/controllers/line_controller.rb
@@ -19,19 +19,7 @@ class LineController < ApplicationController
     logger.debug("token_type = #{token_type}")
     logger.debug("refresh_token = #{refresh_token}")
 
-    profile_request_headers = {
-        'Authorization' => "#{token_type} #{access_token}"
-    }
-    logger.debug("profile_request_headers #{profile_request_headers.to_s}")
-    profile_response = RestClient.get 'https://api.line.me/v2/profile', profile_request_headers}
 
-    @profile_response_json = profile_response.body
-    @userId = @profile_response_json['userId']
-    @display_name = @profile_response_json['displayName']
-    @picture_url = @profile_response_json['pictureUrl']
-    logger.debug("@userId = #{@userId}")
-    logger.debug("@display_name = #{@display_name}")
-    logger.debug("@picture_url = #{@picture_url}")
   end
 
 end
