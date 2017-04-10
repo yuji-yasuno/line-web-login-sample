d6820fb121dde14e8bc1babba00e6a47f8818d08
楊野勇智 <yuji-1124-1984@Mac-Pro.local>
2017-04-10 21:15:28 +0900

    test in heroku

--- app/controllers/line_controller.rb
+++ app/controllers/line_controller.rb
@@ -1,4 +1,16 @@
 class LineController < ApplicationController
+
   def index
   end
+
+  def loggedin
+    code = params[:code]
+    state = params[:test]
+    logger.debug("code = #{code}")
+    logger.debug("state = #{state}")
+    request_body = "grant_type=authorization_code&code=#{code}&client_id=#{ENV['client_id']}&client_secret=#{ENV['client_secret']}&redirect_uri=#{ENV['redirect_uri']}"
+    response = RestClient.post 'https://api.line.me/v2/oauth/accessToken', request_body, {content_type: 'application/x-www-form-urlencoded'}
+    @response_json = response.body
+  end
+
 end
